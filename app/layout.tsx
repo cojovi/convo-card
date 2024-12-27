797674a0fd75ba0b3ab3fd57ca974140d4038aab
cojovi <31863818+cojovi@users.noreply.github.com>
2024-12-27 12:14:51 -0600

    pl

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${poppins.variable} font-sans`}>
         <ThemeProvider defaultTheme="dark">
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,7 @@
 
 import React from 'react'
 import Questionnaire from './questionnaire'
-import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-
   return <Questionnaire />
 }
